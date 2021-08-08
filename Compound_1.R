83ce32a876d94ccfbf875c2ec5b29309ef2dccab
YijingXin0 <88603706+YijingXin0@users.noreply.github.com>
2021-08-08 11:54:50 +0800

    Create Compound_1.R

---
+++ Compound_1.R
@@ -0,0 +1,26 @@
+rm(list=ls())
+gc()
+
+setwd('K:\\compound_1')
+getwd()
+
+expreset <- read.table('compound_1.txt',header = T)
+expreset_agg <- aggregate(x = expreset[,2:7],by = list(expreset$gene_id),FUN = mean)
+rownames(expreset_agg) <- expreset_agg$Group.1
+expreset_agg <- expreset_agg[,-1]
+expreset_agg_round <- round(expreset_agg)
+condition <- factor(rep(c('control', 'treat'), each = 3))
+coldata <- data.frame(row.names=colnames(expreset_agg_round), condition)
+
+library(DESeq2)
+dds <- DESeqDataSetFromMatrix(expreset_agg_round, DataFrame(condition), design= ~ condition )
+head(dds)
+dds <- DESeq(dds)
+res <- results(dds)
+summary(res)
+
+diff_gene_deseq2 <-subset(res,pvalue< 0.05 & (log2FoldChange > 0.584962500721156 | log2FoldChange < -1))
+a <- data.frame(diff_gene_deseq2, stringsAsFactors = FALSE, check.names = FALSE)
+b <- data.frame(res,stringsAsFactors = FALSE, check.names = FALSE)
+write.table(a, 'compound_1.DESeq2.pvalue 0.05_log2FoldChange_0.584962500721156_log2FoldChange_-1.txt', col.names = NA, sep = '\t', quote = FALSE)
+write.table(b, 'compound_1.DESeq2.all.txt', col.names = NA, sep = '\t', quote = FALSE)
