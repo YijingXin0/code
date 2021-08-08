5356944d2c40452cd100ee02eeeecce136961458
YijingXin0 <88603706+YijingXin0@users.noreply.github.com>
2021-08-08 12:35:59 +0800

    Delete Limma.R

--- Limma.R
+++
@@ -1,9 +0,0 @@
-library(DESeq2)
-
-dds <- DESeqDataSetFromMatrix(expreset_agg_round, DataFrame(condition), design= ~ condition )
-head(dds)
-dds <- DESeq(dds)
-res <- results(dds)
-summary(res)
-gene_symbol <- read.csv("Data\\gene_symbol_all_22268.csv")
-sig_id <- read.csv("sigid_A549.csv",header=FALSE)
